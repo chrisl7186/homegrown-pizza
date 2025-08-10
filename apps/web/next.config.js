9e640a4ce01751bf9486f1870faae281060a97c9
Christopher Leo <chrisl7186@gmail.com>
2025-08-10 02:29:31 -0400

    remove turbopack causing web apps repo to crash

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,4 +1,18 @@
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    // 1) Allow Next to trace files outside apps/web (monorepo)
+  outputFileTracingRoot: path.resolve(__dirname, '../../'),
+
+  // 2) Tell Turbopack what you consider the project root (absolute path)
+  turbopack: {
+    root: path.resolve(__dirname, '../../'),
+  },
+};
 
 export default nextConfig;
