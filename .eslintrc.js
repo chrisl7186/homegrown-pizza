1c042e8f70e66449785a349e274f59e80e15f6d5
Christopher Leo <chrisl7186@gmail.com>
2025-08-10 17:51:49 -0400

    update monorepo to work with shadcn

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+// This configuration only applies to the package manager root.
+/** @type {import("eslint").Linter.Config} */
+const config = {
+  ignorePatterns: ["apps/**", "packages/**"],
+  extends: ["@workspace/eslint-config/library.js"],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    project: true,
+  },
+}
+
+export default config
--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,18 +1,12 @@
-import path from 'node:path';
-import { fileURLToPath } from 'node:url';
+// import path from 'node:path';
+// import { fileURLToPath } from 'node:url';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = path.dirname(__filename);
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    // 1) Allow Next to trace files outside apps/web (monorepo)
-  outputFileTracingRoot: path.resolve(__dirname, '../../'),
-
-  // 2) Tell Turbopack what you consider the project root (absolute path)
-  turbopack: {
-    root: path.resolve(__dirname, '../../'),
-  },
+  transpilePackages: ["@workspace/ui"],
 };
 
 export default nextConfig;
